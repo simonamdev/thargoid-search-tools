a1b43527db35b9bd5c01998c29e1e177add634ed
Simon AM <simon@agius-muscat.net>
2017-06-30 23:59:38 +0200

    Added API route

--- possible-new-sites/web.py
+++ possible-new-sites/web.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, send_from_directory, Markup, redirect
+import json
+
+from flask import Flask, render_template, send_from_directory, Markup, redirect, jsonify
 from data import DataRetriever
 from flask import request
 
@@ -9,6 +11,10 @@ data_retriever = DataRetriever()
 
 def parse_radius(amount):
     return round(float(amount), 2)
+
+
+def get_closest_systems(x, y, z, radius=5):
+    return data_retriever.get_closest_systems(x=x, y=y, z=z, radius=radius)
 
 
 @app.route('/')
@@ -22,9 +28,23 @@ def search():
         print('Incorrect values passed: {}'.format(request.form))
         return redirect('/')
     coordinates = [parse_radius(request.form['x']), parse_radius(request.form['y']), parse_radius(request.form['z'])]
-    sites = data_retriever.get_closest_systems(x=coordinates[0], y=coordinates[1], z=coordinates[2], radius=5)
+    sites = get_closest_systems(coordinates[0], coordinates[1], coordinates[2])
     print('Results: {}'.format(sites))
     return render_template('results.html', sites=sites)
+
+
+@app.route('/api/search', methods=['POST'])
+def search_api():
+    if '' in request.form.values():
+        print('Incorrect values passed: {}'.format(request.form))
+        return redirect('/')
+    coordinates = [parse_radius(request.form['x']), parse_radius(request.form['y']), parse_radius(request.form['z'])]
+    sites = get_closest_systems(coordinates[0], coordinates[1], coordinates[2])
+    return jsonify(
+        {
+            'sites': sites
+        }
+    )
 
 
 if __name__ == '__main__':
