295f6c45b7b115af4816feeb70f19a1b799fe6cb
Simon AM <simon@agius-muscat.net>
2017-06-30 21:44:36 +0200

    Removed commented line

--- possible-new-sites/web.py
+++ possible-new-sites/web.py
@@ -24,5 +24,4 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9000, debug=True)
-    # app.run(host='0.0.0.0', port=9000, debug=False)
+    app.run(host='127.0.0.1', port=3000, debug=True)
