ecf05b2b9e91f3cc01a4a198770a5c0e861dcc5e
Simon AM <simon@agius-muscat.net>
2017-07-01 20:31:12 +0200

    Load data from file

--- unknown-sites-body-data/api.py
+++ unknown-sites-body-data/api.py
@@ -1,14 +1,27 @@
 import argparse
 
 from flask import Flask, jsonify, request
+from flask import json
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
+data = {}
+data_file_path = 'us_data.json'
+
+
+def load_data():
+    with open(data_file_path, 'r') as data_file:
+        global data
+        data = json.loads(data_file.read())
 
 
 @app.route('/api/v1/unknown-site')
 def unknown_site_data():
-    return 'Foo'
+    return jsonify(
+        {
+            'data': data
+        }
+    )
 
 
 if __name__ == '__main__':
@@ -20,6 +33,7 @@ if __name__ == '__main__':
         default=False,
         help='Run in debug mode')
     args = parser.parse_args()
+    load_data()
     port = 10000
     if args.debug:
         app.run(host='127.0.0.1', port=port, debug=True, threaded=True)
