ee94689a97ab9a83e808139bbe7de04bde67f331
Simon AM <simon@agius-muscat.net>
2017-07-01 16:39:38 +0200

    Added semi-working example

--- system-map-ocr/parse_image.py
+++ system-map-ocr/parse_image.py
@@ -1,16 +1,25 @@
 import os
 
 import pytesseract
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image
 
 map_image_path = os.path.join(os.path.dirname(__file__), 'map.png')
 temp_image_path = os.path.join(os.path.dirname(__file__), 'temp.jpg')
 
-im = Image.open(map_image_path)
-im = im.filter(ImageFilter.MedianFilter())
-enhancer = ImageEnhance.Contrast(im)
-im = enhancer.enhance(2)
-im = im.convert('1')
-im.save(temp_image_path)
+img = Image.open(map_image_path)
+width, height = img.size
+x = 10
+y = 725
+cropped_img = img.crop(
+    (
+        x,
+        y,
+        363 + x,
+        164 + y
+    )
+)
+cropped_img.show()
+cropped_img.save(temp_image_path)
+
 text = pytesseract.image_to_string(Image.open(temp_image_path))
 print(text)
